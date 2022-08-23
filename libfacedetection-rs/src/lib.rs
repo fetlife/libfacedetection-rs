a33d7e603a539bbd38a2ba07cd5895f290b31b70
Andrii Dmytrenko <andrii@fetlife.com>
2022-08-23 16:04:49 +0100

    Add tests

--- libfacedetection-rs/src/lib.rs
+++ libfacedetection-rs/src/lib.rs
@@ -38,10 +38,10 @@ impl Face {
         let y = *data.offset(2);
         let width = *data.offset(3);
         let height = *data.offset(4);
-        let mut landmarks = [(0,0); 5];
+        let mut landmarks = [(0, 0); 5];
         for idx in 0..5 {
-            let landmark_x = *data.offset(5 + idx*2);
-            let landmark_y = *data.offset(5 + idx*2+1);
+            let landmark_x = *data.offset(5 + idx * 2);
+            let landmark_y = *data.offset(5 + idx * 2 + 1);
             let landmark = (landmark_x, landmark_y);
             landmarks[idx as usize] = landmark;
         }
@@ -96,4 +96,25 @@ pub fn facedetect_cnn(
     }
     let detection_result = DetectionResult { faces };
     Ok(detection_result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::facedetect_cnn;
+    use opencv::{imgcodecs::IMREAD_COLOR, prelude::MatTraitConst};
+    use opencv::prelude::*;
+
+    #[test]
+    fn test_detection() {
+        let img = opencv::imgcodecs::imread("tests/face.jpg", IMREAD_COLOR)
+            .expect("failed to load image");
+        let faces = facedetect_cnn(
+            img.ptr(0).unwrap(),
+            img.cols(),
+            img.rows(),
+            img.mat_step().get(0) as u32,
+        )
+        .expect("failed to detect faces");
+        assert_eq!(faces.faces.len(), 1);
+    }
 }
